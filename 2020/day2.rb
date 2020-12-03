99bfb982ecb24b99708973f2baff5f5c3feed707
Jason Morrow <jason@opslevel.com>
2020-12-03 10:05:41 -0500

    Day 2 Part 2

--- 2020/day2.rb
+++ 2020/day2.rb
@@ -11,5 +11,18 @@ def check_policy(line)
 end
 
 File.foreach("day2_input.txt") { |line| count += 1 if check_policy(line) }
-
 puts "Part 1: #{count}"
+
+def new_policy(line)
+  split_line = line.split(' ')
+  min, max = split_line[0].split('-').map(&:to_i)
+  letter = split_line[1][0]
+  pass = split_line[2]
+
+  (pass[min-1] == letter) ^ (pass[max-1] == letter)
+end
+
+count = 0
+File.foreach("day2_input.txt") { |line| count += 1 if new_policy(line) }
+puts "Part 2: #{count}"
+
